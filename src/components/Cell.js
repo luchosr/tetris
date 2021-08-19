d7f4efb3ce5813e342902e4fa24b1cf76f7816d4
Luciano <luchosr@gmail.com>
2021-08-19 12:48:00 +0200

    update components

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,7 +3,7 @@ import { StyledCell } from "./styles/StyledCell";
 import { TETROMINOS } from "../tetrominos";
 
 const Cell = ({ type }) => {
-  return <StyledCell tyoe={"L"} color={TETROMINOS["L"].color} />;
+  return <StyledCell type={"L"} color={TETROMINOS["L"].color} />;
 };
 
 export default Cell;
--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,16 +1,12 @@
 import React from "react";
-
 import { StyledStage } from "./styles/StyledStage";
+
 import Cell from "./Cell";
 
-const Stage = ({ stage }) => {
-  return (
-    <StyledStage with={stage[0].length} height={stage.length}>
-      {stage.map((row) =>
-        row.map((cell, x) => <Cell key={x} type={cell[0]} />)
-      )}
-    </StyledStage>
-  );
-};
+const Stage = ({ stage }) => (
+  <StyledStage width={stage[0].length} height={stage.length}>
+    {stage.map((row) => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
+  </StyledStage>
+);
 
 export default Stage;
