a7c34a6f102b323c58aa93d9e6807274bd196ede
Luciano <luchosr@gmail.com>
2021-08-23 21:01:18 +0200

    update hooks

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -22,6 +22,19 @@ export const usePlayer = () => {
   const playerRotate = (stage, dir) => {
     const clonedPlayer = JSON.parse(JSON.stringify(player));
     clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+
+    const pos = clonedPlayer.pos.x;
+    let offset = 1;
+    while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+      clonedPlayer.pos.x += offset;
+      offset = -(offset + (offset > 0 ? 1 : -1));
+      if (offset > clonedPlayer.tetromino[0].length) {
+        rotate(clonedPlayer.tetromino, -dir);
+        clonedPlayer.pos.x = pos;
+        return;
+      }
+    }
+
     setPlayer(clonedPlayer);
   };
 
