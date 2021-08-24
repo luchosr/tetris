432396a9e2122fcb30df6f6429078b79d54dd0ca
Luciano <luchosr@gmail.com>
2021-08-24 17:44:53 +0200

    update cell component

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,5 +1,5 @@
 import { useState, useCallback } from "react";
-import { STAGE_WIDTH } from "../gameHelpers";
+import { STAGE_WIDTH, checkCollision } from "../gameHelpers";
 import { TETROMINOS, randomTetromino } from "../tetrominos";
 
 export const usePlayer = () => {
