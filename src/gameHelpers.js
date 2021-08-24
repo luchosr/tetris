e07bd3da796e19c825e5bbfd06e7fb10b308330d
Luciano <luchosr@gmail.com>
2021-08-24 19:55:34 +0200

    update game helpers

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -5,18 +5,22 @@ export const createStage = () =>
   Array.from(Array(STAGE_HEIGHT), () =>
     new Array(STAGE_WIDTH).fill([0, "clear"])
   );
+
 export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
-  return player.tetromino.some((row, y) =>
-    row.some((cell, x) => {
-      if (cell !== 0) {
-        return (
+  for (let y = 0; y < player.tetromino.length; y += 1) {
+    for (let x = 0; x < player.tetromino[y].length; x += 1) {
+      if (player.tetromino[y][x] !== 0) {
+        if (
           !stage[y + player.pos.y + moveY] ||
           !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
           stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
             "clear"
-        );
+        ) {
+          return true;
+        }
       }
-      return false;
-    })
-  );
+    }
+  }
+
+  return false;
 };
