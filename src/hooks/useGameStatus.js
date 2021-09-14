67fe6eb1c4dd2824e574e153f298089c0ee74742
Luciano <luchosr@gmail.com>
2021-09-14 22:23:17 +0200

    update useGameStatus

--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -5,8 +5,8 @@ export const useGameStatus = (rowsCleared) => {
   const [rows, setRows] = useState(0);
   const [level, setLevel] = useState(0);
 
-  const linePoints = [40, 100, 300, 1200];
   const calcScore = useCallback(() => {
+    const linePoints = [40, 100, 300, 1200];
     if (rowsCleared > 0) {
       setScore((prev) => prev + linePoints[rowsCleared - 1] * (level + 1));
       setRows((prev) => prev + rowsCleared);
