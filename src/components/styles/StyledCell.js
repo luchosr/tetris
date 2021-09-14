08c1039ec50e4b4ba73c883cfcb36bfb39696900
Luciano <luchosr@gmail.com>
2021-09-14 21:32:26 +0200

    styled cell modified

--- src/components/styles/StyledCell.js
+++ src/components/styles/StyledCell.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const StyledCell = styled.div`
   width: auto;
-  /* border-radius: 5px; */
   background: rgba(${(props) => props.color}, 0.8);
   border: ${(props) => (props.type === 0 ? "0px solid" : "4px solid")};
   border-bottom-color: rgba(${(props) => props.color}, 0.1);
